b33ab1d8d7a0d82234d941b97909594576ad84ea
michacurri <mkcurri@gmail.com>
2020-06-11 18:14:29 -0400

    organized AirVisual API calls to collect Array of City Data

--- apps/apps.js
+++ apps/apps.js
@@ -10,40 +10,61 @@ app.mapApiKey = 'EPSBNGNeFxHPINGRYzr7X1Sgyh8vyQpV';
 //Creating a function for making a call to the Visual Air Api (aka ajax call) to get the cities array
 app.getCitiesArray = (state) => {
     $.ajax({
-        url: `https://api.airvisual.com/v2/cities`,
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            state: state,
-            country: 'Canada',
-            key: app.airApiKey,
-        }
-    }).then(function(response) {
-        console.log('Yay, I got a response', response);
-    })
-    .fail(function() {
-        alert('Sorry, cities cannot be found');
-    });
+            url: `https://api.airvisual.com/v2/cities`,
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                state: state,
+                country: 'Canada',
+                key: app.airApiKey,
+            }
+        }).then(function (response) {
+            console.log('Yay, I got a response', response.data[0]);
+            // const provResp = response.data[0].city;
+            const citiesArray = [];
+            response.data.forEach((arrayItem) => {
+                citiesArray.push(arrayItem.city);
+                $('ul').append(`<li><span class="fa fa-square-o"></span>${arrayItem.city}</li>`);
+                // console.log(arrayItem.city);
+            });
+            console.log(citiesArray);
+        })
+        .fail(function () {
+            alert('Sorry, cities cannot be found');
+        });
 };
+
+// app.getLatLng = (citiesArray) => {
+
+// }
+
+// creating a function to pass the clicked city to the API for Air Quality
+app.grabLiText = function() {
+    let chosenCity = $(this).text();
+    console.log(chosenCity);
+    app.getCityData(chosenCity, selectedProvince);
+};
+
 
 //Creating a function for making a call to the Visual Air Api (aka ajax call) to get data on a specific city
 app.getCityData = (city, state) => {
     $.ajax({
-        url: `https://api.airvisual.com/v2/city`,
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            city: city,
-            state: state,
-            country: 'Canada',
-            key: app.airApiKey,
-        }
-    }).then(function(response) {
-        console.log('Yay, I got a response', response.data.current.pollution.aqius);
-    })
-    .fail(function() {
-        alert('Sorry, your city cannot be found');
-    });
+            url: `https://api.airvisual.com/v2/city`,
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                city: city,
+                state: state,
+                country: 'Canada',
+                key: app.airApiKey,
+            }
+        }).then(function (response) {
+            // console.log('Yay, I got a response', response.data.current.pollution.aqius);
+            console.log(`The air quality in ${city}, ${state} is ${response.data.current.pollution.aqius}. The current temperature is ${response.data.current.weather.tp} degrees Celcius.`);
+        })
+        .fail(function () {
+            alert('Sorry, your city cannot be found');
+        });
 };
 
 let selectedProvince;
@@ -52,43 +73,46 @@ app.grabInput = () => {
     alert("You have selected the province - " + selectedProvince);
     console.log(selectedProvince);
     app.getCitiesArray(selectedProvince);
-    app.getCityData('Toronto', selectedProvince);
+    // app.getCityData('Toronto', selectedProvince);
+
 
 };
 
 
 //Creating an init function
-app.init = function() {
+app.init = function () {
     $(".province").change(app.grabInput);
+    $('ul').on('click', 'li', app.grabLiText);
+
 
     /////////////////////////
     // LEAFLET MAP
     const map = L.map('map').setView([58, -100], 4);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     L.marker([55, -100]).addTo(map)
-      .bindPopup('Canada.<br> Air Quality App.')
-      .openPopup();
+        .bindPopup('Canada.<br> Air Quality App.')
+        .openPopup();
 
-      L.marker([43.651893, -79.381713]).addTo(map)
-      .bindPopup('Toronto')
-      .openPopup();
+    L.marker([43.651893, -79.381713]).addTo(map)
+        .bindPopup('Toronto')
+        .openPopup();
 
-      L.marker([45.420421, -75.692432]).addTo(map)
-      .bindPopup('Ottawa')
-      .openPopup();
+    L.marker([45.420421, -75.692432]).addTo(map)
+        .bindPopup('Ottawa')
+        .openPopup();
 
-      L.marker([49.900496, -97.139309]).addTo(map)
-      .bindPopup('Winnipeg')
-      .openPopup();
+    L.marker([49.900496, -97.139309]).addTo(map)
+        .bindPopup('Winnipeg')
+        .openPopup();
     /////////////////////////
 
 };
 
 //Creating document ready
-$(document).ready(function() {
+$(document).ready(function () {
     app.init();
 });
